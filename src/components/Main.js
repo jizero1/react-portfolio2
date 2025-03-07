54b61be2d776c458b3c72bc06e9dd09f948036ca
최지영 <jiyoung@choejiyeong-ui-MacBookAir.local>
2025-03-07 18:31:05 +0900

    메인화면에서 scroll icon추가 및 animation추가

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import './Main.css';
 import { motion } from "framer-motion";
 // import Memu from './Menu';
 import Menu from './Menu.js';
-
+import { FaAngleDoubleDown } from "react-icons/fa";
 
 // framer motion - 메인 소개 화면 문구 띄우기
 const MainIntroduceAni = ({ initial, animate, duration, delay, children }) => {
@@ -20,14 +20,30 @@ const MainIntroduceAni = ({ initial, animate, duration, delay, children }) => {
 const MainIntroduce = () => {
     return (
         <div className="main-introduce common-flex">
-            {/* <p className="main-introduce-name">안녕하세요</p> */}
-            {/* <MainIntroduceAni delay={0.8} className="main-introduce-heading">
-                안녕하세요.
+            <MainIntroduceAni
+                initial={{opacity: 0, y: 100}}
+                animate={{opacity: 1, y: 0}}
+                duration="2"
+                delay="0.5">
+                <p className="main-introduce-text">안녕하세요, <br/> 프론트엔드 개발자 <span className="main-introduce-text-name">최지영</span>입니다.</p>
             </MainIntroduceAni>
-            <MainIntroduceAni delay={5} className="main-introduce-heading">
-                프론트엔드 개발자 최지영입니다.
-            </MainIntroduceAni> */}
+            
         </div>
+    )
+}
+
+const MainScrollIcon = () => {
+    return (
+        <MainIntroduceAni
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            duration="1"
+            delay="2"
+            // className="main-scroll"
+        >
+            {/* <p className="main-scroll">dd</p> */}
+            <FaAngleDoubleDown className="main-scroll"/>
+        </MainIntroduceAni>
     )
 }
 const Main = () => {
@@ -35,6 +51,7 @@ const Main = () => {
         <div id="home" className="main-container common-flex">
             <MainIntroduce/>
             <Menu />
+            <MainScrollIcon/>
         </div>
     )
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,27 @@
 import './Menu.css';
 import { Link, Element } from 'react-scroll';
+import {motion} from 'framer-motion';
+import React, { useState } from 'react';
 
 const MenuNav = () => {
+    const [fontSize, setFontSize] = useState(0);
+    const handleAnimationComplete = () => {
+        setFontSize(18);
+    }
     return (
-            <nav className="menu-nav common-flex">
+        <motion.div
+        initial={{ opacity: 0, width: 0, fontSize: 0}}
+        animate={{ opacity: 1, width: 300}}
+        transition={{duration: 1, delay: 1.5}}
+        onAnimationComplete={handleAnimationComplete}
+        className="menu-nav common-flex"
+        >
+            <ul className="menu-ul common-flex" style={{fontSize: fontSize}}>
                 <li className="nav-home common-nav"><Link to="home" smooth={true} duration={500}>홈</Link></li>
                 <li className="nav-aboutMe common-nav"><Link to="about" smooth={true} duration={500}>자기소개</Link></li>
                 <li className="nav-project common-nav"><Link to="project" smooth={true} duration={500}>프로젝트</Link></li>
-            </nav>
+            </ul>
+            </motion.div>
     )
 }
 const Menu = () => {
