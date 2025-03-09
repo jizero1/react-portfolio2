e5ff8a2f7ceb7eabeb524eee119f662704ac861a
최지영 <jiyoung@choejiyeong-ui-MacBookAir.local>
2025-03-09 23:31:37 +0900

    skill을 map함수로 나열하여 코드 중복을 줄임

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -8,19 +8,26 @@ const SkillTitle = () => {
     )
 }
 const SkillImg = () => {
+    const skills = [
+        {name: "HTML5", icon: "/images/html5-Icon.svg"},
+        {name: "CSS3", icon: "/images/css3-Icon.svg"},
+        {name: "JavaScript", icon: "/images/javaScript-Icon.svg"},
+        {name: "React", icon: "/images/react-Icon.svg"},
+        {name: "ReactNative", icon: "/images/reactNative-Icon.svg"},
+        {name: "Redux", icon: "/images/redux-Icon.svg"},
+        {name: "GitHub", icon: "/images/github-Icon.svg"}
+    ];
     return (
         <div className="skillImg-container">
-            <p className="common-skill-text common-flex"><img className="common-skill-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"/>HTML5</p>
-
-            <p className="common-skill-text common-flex"><img className="common-skill-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"/>CSS3</p>
-            <p className="common-skill-text common-flex"><img className="common-skill-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />JavaScript</p>
-
-            <p className="common-skill-text common-flex"><img className="common-skill-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />React</p>
-          
-            <p className="common-skill-text common-flex"><img className="common-skill-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redux/redux-original.svg" />Redux</p>
-            <p className="common-skill-text common-flex"><img className="common-skill-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />Github</p>
-          
-          
+            {skills.map((skill,index) => (
+                <p key={index} className="common-skill-text common-flex">
+                    <img className="common-skill-img"
+                        src={skill.icon}
+                        alt={`${skill.name}Icon`}
+                    />
+                    {skill.name}
+                </p>
+            ))}
         </div>
     )
 }
