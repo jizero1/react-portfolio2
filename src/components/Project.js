3c460c17dabfe4aa91a1e9aa927a5bf34d63f4fe
최지영 <jiyoung@choejiyeong-ui-MacBookAir.local>
2025-03-11 23:10:36 +0900

    skill페이지 height수정

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,16 @@
 import './Project.css';
 
+const ProjectTitle = () => {
+    return (
+        <div className="projectTitle-container common-flex">
+            <p>project</p>
+        </div>
+    )
+}
 const Project = () => {
     return (
         <div id="project" className="project-container">
-
+            <ProjectTitle/>
         </div>
     )
 }
