27866449e3f5a88419101175f51ce5fa7f048ce1
최지영 <jiyoung@choejiyeong-ui-MacBookAir.local>
2025-03-15 23:27:40 +0900

    project.js 날씨프로젝트 about 추가

--- src/components/Project.js
+++ src/components/Project.js
@@ -10,8 +10,8 @@ const ProjectTitle = () => {
 
 const ProjectCard = () => {
     const card = [
-        { projectName: "퍼스널컬러 테스트", projectAbout: "퍼스널컬러에 대한 소개 및 테스트를 할 수 있는 페이지 입니다. React Router을 사용하여 페이지간 이동을 관리 하였고, 테스트는 큰 틀로 웜/쿨로 나누어 테스트 후, 세부테스트로 이동하여 테스트의 정확도를 높이고자 하였습니다.", className: "projectCard-img common-img", img: "/images/personal.png", alt: "퍼스널컬러 테스트 이미지" },
-        { projectName: "실시간 날씨", projectAbout: "", className: "projectCard-img common-img", img: "/images/weather.png", alt: "실시간 날씨 이미지" },
+        { projectName: "퍼스널컬러 테스트", projectAbout: "퍼스널컬러에 대한 소개 및 테스트를 할 수 있는 페이지 입니다. React Router을 사용하여 페이지간 이동을 관리 하였고, 테스트는 웜/쿨처럼 큰 틀로 나누어 테스트 진행 후, 세부테스트로 이동하여 테스트의 정확도를 높이고자 하였습니다.", className: "projectCard-img common-img", img: "/images/personal.png", alt: "퍼스널컬러 테스트 이미지" },
+        { projectName: "실시간 날씨", projectAbout: "OpenWeather API를 이용하여 사용자의 현재위치 기반으로 날씨 정보를 불러오는 페이지 입니다. 날씨 상태에 따라 날씨 이모티콘을 변경하여 사용자가 쉽게 이해 하고 확인 할 수 있도록 하였고, 비동기 데이터 관리와 API연동을 위해 node.js를 사용하여 서버를 구축 하였습니다.", className: "projectCard-img common-img", img: "/images/weather.png", alt: "실시간 날씨 이미지" },
         { projectName: "달력 일기", projectAbout: "", className: "projectCard-img common-img", img: "/images/calendar.png", alt: "일기 캘린더 이미지" },
         { projectName: "하루토마토 (ToDo App)", projectAbout: "", className: "projectCard-AppImg common-img", img: "/images/todoApp.jpeg", alt: "투두앱 이미지" }
     ]
