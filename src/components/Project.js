fbdcd573e3119067c8aa4b4aee1f414b431fc26f
최지영 <jiyoung@choejiyeong-ui-MacBookAir.local>
2025-03-18 23:55:42 +0900

    projectCard에 마우스 올리면 프로젝트페이지 이동 및 github이동 하는 버튼 보여줌

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,5 @@
 import './Project.css';
-
+import React, { useState} from 'react';
 const ProjectTitle = () => {
     return (
         <div className="projectTitle-container common-flex">
@@ -9,15 +9,29 @@ const ProjectTitle = () => {
 }
 
 const ProjectCard = () => {
+
+    const handleClick = (url) => {
+        if (url === "") {
+            alert("이 프로젝트는 private로 비공개 되어있습니다.");
+        } else {
+            window.open(url,"_blank");
+        }
+    }
+
+
     const card = [
         { id: 0, projectName: "퍼스널컬러 테스트", projectAbout: "퍼스널컬러에 대한 소개 및 테스트를 할 수 있는 페이지 입니다. React Router을 사용하여 페이지간 이동을 관리 하였고, 테스트는 웜/쿨처럼 큰 틀로 나누어 테스트 진행 후, 세부테스트로 이동하여 테스트의 정확도를 높이고자 하였습니다.",
-             skill: ['React','React Router'], className: "projectCard-img common-img", img: "/images/personal.png", alt: "퍼스널컬러 테스트 이미지" },
+             skill: ['React','React Router'], className: "projectCard-img common-img", img: "/images/personal.png", alt: "퍼스널컬러 테스트 이미지",
+            pageLink: "https://jizero1.github.io/project-personal/", gitHubLink: "https://github.com/jizero1/project-personal" },
         { id: 1, projectName: "실시간 날씨", projectAbout: "OpenWeather API를 이용하여 사용자의 현재위치 기반으로 날씨 정보를 불러오는 페이지 입니다. 날씨 상태에 따라 날씨 이모티콘을 변경하여 사용자가 쉽게 이해 하고 확인 할 수 있도록 하였고, 비동기 데이터 관리와 API연동을 위해 node.js를 사용하여 서버를 구축 하였습니다.",
-            skill: ['JavaScript','OpenWeather API', 'Node.js'], className: "projectCard-img common-img", img: "/images/weather.png", alt: "실시간 날씨 이미지" },
+            skill: ['JavaScript','OpenWeather API', 'Node.js'], className: "projectCard-img common-img", img: "/images/weather.png", alt: "실시간 날씨 이미지",
+            pageLink: "https://weather-project-peach-xi.vercel.app", gitHubLink: "https://github.com/jizero1/weather" },
         { id: 2, projectName: "달력 일기", projectAbout: "현재 날짜를 확인하고, 이전달/다음달로 이동하는 달력 기능에 일기 기능을 더한 페이지 입니다. 날짜 클릭시 해당 날짜에 이미지와 텍스트를 추가하여 일기처럼 달력을 사용 할 수 있습니다. 또한, localStorage로 데이터를 관리 하여 새로고침 하거나 달력 이동시에도 데이터가 유지 되게 하였습니다.",
-            skill: ['JavaScript','LocalStorage'], className: "projectCard-img common-img", img: "/images/calendar.png", alt: "일기 캘린더 이미지" },
+            skill: ['JavaScript','LocalStorage'], className: "projectCard-img common-img", img: "/images/calendar.png", alt: "일기 캘린더 이미지",
+            pageLink: "https://jizero1.github.io/CalendarDiary/", gitHubLink: "https://github.com/jizero1/CalendarDiary" },
         { id: 3, projectName: "하루토마토 (ToDo App)", projectAbout: "날짜별로 할 일을 기록/관리하는 앱 입니다. 평범한 체크 표시 대신, 토마토 아이콘으로 할 일을 체크할 수 있고, 5가지 색상과 표정의 아이콘으로 기분 표시도 가능 합니다. 또한, AsyncStorage를 이용하여 앱에 데이터가 유지되도록 하였습니다.",
-            skill: ['React','ReactNative','AsyncStorage'], className: "projectCard-AppImg common-img", img: "/images/todoApp.jpeg", alt: "투두앱 이미지" }
+            skill: ['React','ReactNative','AsyncStorage'], className: "projectCard-AppImg common-img", img: "/images/todoApp.jpeg", alt: "투두앱 이미지",
+            pageLink: "https://play.google.com/store/apps/details?id=com.jizerotodo&pli=1", gitHubLink: "" }
     ]
     return (
         <div className="projectCard-container common-flex">
@@ -30,10 +44,10 @@ const ProjectCard = () => {
                         {card.skill.map((skill) => (
                             <p key={skill} className="projectCard-skill common-flex">{skill}</p>
                         ))}
-                        {/* <p className="projectCard-skill common-flex">HTML</p>
-                        <p className="projectCard-skill common-flex">React</p>
-                        <p className="projectCard-skill common-flex">HTML</p>
-                        <p className="projectCard-skill common-flex">HTML</p> */}
+                    </div>
+                    <div className="projectCard-listHover common-flex">
+                        <button><a href={card.pageLink} target='_blank'>자세히 보기</a></button>
+                        <button onClick={() => handleClick(card.gitHubLink)}>GitHub 이동</button>
                     </div>
                 </div>
             ))}
